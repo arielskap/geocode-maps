bab79a2343aae8e7448c12313e6c0593040feeda
Ariel Villarreal <arielskap@gmail.com>
2021-04-06 15:03:32 -0300

    modificando la funcionalidad

--- pages/bigDataCloud.tsx
+++ pages/bigDataCloud.tsx
@@ -1,10 +1,16 @@
 /* eslint-disable react/no-array-index-key */
 import Layout from "@components/Layout"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 let WATCH_ID = NaN
+const GEO_OPTIONS = {
+	enableHighAccuracy: true,
+	maximumAge: 0,
+	timeout: 10000
+}
 
 const BigDataCloud: React.FC = () => {
+	const buttonGelocationRef = useRef<HTMLButtonElement>( null )
 	const [localidades, setLocalidades] = useState<string[]>( [] )
 	const geoSuccess = async ( position: GeolocationPosition ) => {
 		const result = await fetch( `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&localityLanguage=es` ).then( ( result ) => {
@@ -46,15 +52,39 @@ const BigDataCloud: React.FC = () => {
 	}, [localidades] )
 
 	useEffect( () => {
-		if ( `geolocation` in navigator ) {
-			true
+		if ( localidades.length > 3 ) {
+			navigator.geolocation.clearWatch( WATCH_ID )
+		}
+		return navigator.geolocation.clearWatch( WATCH_ID )
+	}, [localidades] )
+
+	useEffect( () => {
+		navigator.permissions.query( {name: `geolocation`} ).then( ( result ) => {
+			const button = buttonGelocationRef.current
+
+			report( result.state )
+			if ( result.state == `granted` ) {
+				WATCH_ID = navigator.geolocation.watchPosition( geoSuccess, geoError, GEO_OPTIONS )
+			} else if ( result.state == `prompt` ) {
+				navigator.geolocation.getCurrentPosition( geoSuccess, geoError, GEO_OPTIONS )
+			} else if ( result.state == `denied` ) {
+				if ( button ) {
+					button.disabled = true
+				}
+			}
+			result.onchange = function() {
+				report( result.state )
+			}
+		} )
+		function report( state: string ) {
+			console.log( `Permission ` + state )
 		}
 	}, [] )
 
 	return (
 		<Layout title="Big Data Cloud Api GeoCode">
 			<div className='flex flex-col items-center justify-center h-screen'>
-				<button className='px-2 py-1 mt-4 text-white bg-blue-600 rounded hover:bg-blue-800' type='button' onClick={handleClickLocalidad}>Obtener posicion actual</button>
+				<button ref={buttonGelocationRef} className='px-2 py-1 mt-4 text-white bg-blue-600 rounded hover:bg-blue-800' type='button' onClick={handleClickLocalidad}>Obtener posicion actual</button>
 				{localidades.map( ( localidad, i ) => {
 					return (
 						<p key={`localidad-${i}`}>Localidad {i + 1}: {localidad}</p>
--- pages/positionStack.tsx
+++ pages/positionStack.tsx
@@ -1,10 +1,15 @@
 /* eslint-disable react/no-array-index-key */
 import Layout from "@components/Layout"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 let WATCH_ID = NaN
-
+const GEO_OPTIONS = {
+	enableHighAccuracy: true,
+	maximumAge: 0,
+	timeout: 10000
+}
 const PositioNStack: React.FC = () => {
+	const buttonGelocationRef = useRef<HTMLButtonElement>( null )
 	const [localidades, setLocalidades] = useState<string[]>( [] )
 	const geoSuccess = async ( position: GeolocationPosition ) => {
 		const result = await fetch( `http://api.positionstack.com/v1/reverse?access_key=999bceccf9d3b41705b1a0312a6d7099&query=${position.coords.latitude},${position.coords.longitude}` ).then( ( result ) => {
@@ -25,14 +30,7 @@ const PositioNStack: React.FC = () => {
 
 	const handleClickLocalidad = () => {
 		if ( `geolocation` in navigator ) {
-			const geoOptions = {
-				enableHighAccuracy: true,
-				maximumAge: 0,
-				timeout: 10000
-			}
-
-			// navigator.geolocation.getCurrentPosition( geoSuccess, geoError, geoOptions )
-			WATCH_ID = navigator.geolocation.watchPosition( geoSuccess, geoError, geoOptions )
+			WATCH_ID = navigator.geolocation.watchPosition( geoSuccess, geoError, GEO_OPTIONS )
 		} else {
 			alert( `Geolocalizacion no esta disponible` )
 		}
@@ -46,15 +44,32 @@ const PositioNStack: React.FC = () => {
 	}, [localidades] )
 
 	useEffect( () => {
-		if ( `geolocation` in navigator ) {
-			true
+		navigator.permissions.query( {name: `geolocation`} ).then( ( result ) => {
+			const button = buttonGelocationRef.current
+
+			report( result.state )
+			if ( result.state == `granted` ) {
+				WATCH_ID = navigator.geolocation.watchPosition( geoSuccess, geoError, GEO_OPTIONS )
+			} else if ( result.state == `prompt` ) {
+				navigator.geolocation.getCurrentPosition( geoSuccess, geoError, GEO_OPTIONS )
+			} else if ( result.state == `denied` ) {
+				if ( button ) {
+					button.disabled = true
+				}
+			}
+			result.onchange = function() {
+				report( result.state )
+			}
+		} )
+		function report( state: string ) {
+			console.log( `Permission ` + state )
 		}
 	}, [] )
 
 	return (
 		<Layout title="Position Stack Api GeoCode">
 			<div className='flex flex-col items-center justify-center h-screen'>
-				<button className='px-2 py-1 mt-4 text-white bg-blue-600 rounded hover:bg-blue-800' type='button' onClick={handleClickLocalidad}>Obtener posicion actual</button>
+				<button ref={buttonGelocationRef} className='px-2 py-1 mt-4 text-white bg-blue-600 rounded hover:bg-blue-800' type='button' onClick={handleClickLocalidad}>Obtener posicion actual</button>
 				{localidades.map( ( localidad, i ) => {
 					return (
 						<p key={`localidad-${i}`}>Localidad {i + 1}: {localidad}</p>
